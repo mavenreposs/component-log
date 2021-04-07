0093b025da3ae2da785c8341d51f6c6d39bc67a5
王正东 <wangzhengdong@ecmoban.com>
2021-04-07 18:56:05 +0800

    书写测试类

---
+++ src/test/java/io/github/mavenreposs/component/log/TestLog.java
@@ -0,0 +1,39 @@
+package io.github.mavenreposs.component.log;
+
+import org.junit.jupiter.api.Test;
+
+public class TestLog {
+
+    @Test
+    public void testLog() {
+        RCLog.init("AAA");
+//        RCLog.init("AAA", 2);
+//        RCLog.init("AAA", 3, LogLevel.INFO);
+//        RCLog.init("AAA", 4, LogLevel.INFO, true);
+
+        //输出Json字符串
+        RCLog.json("{\"id\":221,\"name\":\"my name is ueueo\",\"desc\":\"this is description!\"}");
+        //输出Xml字符串
+        RCLog.xml("<?xml version=\"1.0\" encoding=\"UTF-8\"?><html><title>this is a title</title><body>这个是网页</body></html>");
+
+        //创建Java对象
+        User user = new User();
+        user.id = 102;
+        user.name = "RCLOG";
+        user.age = 22;
+        //输出对象
+        RCLog.object(user);
+    }
+
+
+    private static class User {
+
+        public Integer id;
+
+        public String name;
+
+        public Integer age;
+
+    }
+
+}
