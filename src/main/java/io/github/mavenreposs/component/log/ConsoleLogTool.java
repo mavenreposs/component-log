dbbd2218cec43a8c3405bc4566ed85ae69cb5e0d
王正东 <wangzhengdong@ecmoban.com>
2021-04-08 12:05:42 +0800

    优化代码

--- src/main/java/io/github/mavenreposs/component/log/ConsoleLogTool.java
+++ src/main/java/io/github/mavenreposs/component/log/ConsoleLogTool.java
@@ -1,7 +1,8 @@
 package io.github.mavenreposs.component.log;
 
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 控制台输出Log
@@ -9,35 +10,37 @@ import org.apache.log4j.Logger;
 public class ConsoleLogTool implements LogInterface {
     @Override
     public void d(String tag, String message) {
-        Logger logger = Logger.getLogger(this.getClass());
-        logger.debug(message);
+        Logger logger = LoggerFactory.getLogger(Object.class);
+        logger.info(message);
     }
 
     @Override
     public void e(String tag, String message) {
-        Logger logger = Logger.getLogger(this.getClass());
+        Logger logger = LoggerFactory.getLogger(Object.class);
         logger.error(message);
     }
 
     @Override
     public void w(String tag, String message) {
-        Logger logger = Logger.getLogger(this.getClass());
+        Logger logger = LoggerFactory.getLogger(Object.class);
         logger.warn(message);
     }
 
     @Override
     public void i(String tag, String message) {
-        Logger logger = Logger.getLogger(this.getClass());
+        Logger logger = LoggerFactory.getLogger(Object.class);
         logger.info(message);
     }
 
     @Override
     public void v(String tag, String message) {
-
+        Logger logger = LoggerFactory.getLogger(Object.class);
+        logger.debug(message);
     }
 
     @Override
     public void wtf(String tag, String message) {
-
+        Logger logger = LoggerFactory.getLogger(Object.class);
+        logger.debug(message);
     }
 }
